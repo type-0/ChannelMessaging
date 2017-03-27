cc0c7c1fc0bf3eefe1e635e24185fe981ae5a6d0
revetr <revet.romain@gmail.com>
2017-03-27 11:03:10 +0200

    Gestion erreur de connexion

--- app/src/main/java/example/revetr/channelmessaging/LoginActivity.java
+++ app/src/main/java/example/revetr/channelmessaging/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -69,24 +70,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Gson gson = new Gson();
         Log.e(TAG, "onDownloadCompleted: " + content );
         Response reponse = gson.fromJson(content, Response.class);
-        if(reponse.getAccesstoken() != null){
-            SharedPreferences settings = getSharedPreferences("carcajou",0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putString("accesstoken",reponse.getAccesstoken());
-            editor.commit();
-            Intent loginIntent = new Intent(LoginActivity.this, ChannelListActivity.class);
-            startActivity(loginIntent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, findViewById(R.id.logo), "logo").toBundle());
-            /*Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
-            startActivity(myIntent);*/
+        if(reponse != null){
+            if(reponse.getAccesstoken() != null){
+                SharedPreferences settings = getSharedPreferences("carcajou",0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("accesstoken",reponse.getAccesstoken());
+                editor.commit();
+                Intent loginIntent = new Intent(LoginActivity.this, ChannelListActivity.class);
+                startActivity(loginIntent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, findViewById(R.id.logo), "logo").toBundle());
+                /*Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
+                startActivity(myIntent);*/
+            }
+            else{
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, R.string.wrong_login, duration);
+                toast.show();
+            }
         }
         else{
-            Context context = getApplicationContext();
-            CharSequence text = "Informations de connexion erronées";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(R.id.llBackground), R.string.no_internet, Snackbar.LENGTH_LONG);
+            snackbar.show();
         }
-
     }
 }
--- app/src/main/java/example/revetr/channelmessaging/RecordSoundDialogFragment.java
+++ app/src/main/java/example/revetr/channelmessaging/RecordSoundDialogFragment.java
@@ -1,25 +1,36 @@
 package example.revetr.channelmessaging;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.Button;
+
+import java.io.IOException;
 
 /**
  * Created by revetr on 15/03/2017.
  */
 public class RecordSoundDialogFragment extends DialogFragment {
+
+    private Button recordButton;
+    private Button playButton;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.sound_record, null));
+        /*recordButton =(Button) findViewById(R.id.recordbutton);
+        recordButton.setOnClickListener(this);
+        playButton =(Button) findViewById(R.id.playbutton);
+        playButton.setOnClickListener(this);*/
         builder.setMessage(R.string.dialog_record_sound)
-                .setPositiveButton(R.string.record, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
-                    }
-                })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // User cancelled the dialog
